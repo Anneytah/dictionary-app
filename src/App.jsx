16e74f78239c4f873d0b61e3c1f1e6287bb15deb
Anneytah <aniagoanita@gmail.com>
2025-04-16 05:16:57 -0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center p-20 bg-cyan-500">
+         
+      <h1 className='text-red-400 font-bold text-4xl p-3'>DICTIONARY APP</h1>
+     
+      <p className="read-the-docs text-[#888]">
+        Welcome to my Dictionary Project with React
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
